5ae80b5e93e48ebcfe35e68549014524497ba689
Ivan Rozanov <navi.zor9@gmail.com>
2023-03-04 23:31:42 +0300

    add Cors configuration

--- CompanyEmployees/Startup.cs
+++ CompanyEmployees/Startup.cs
@@ -1,3 +1,4 @@
+using CompanyEmployees.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +26,7 @@ namespace CompanyEmployees
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureCors();
 
             services.AddControllers();
         }
