e3f1daf7d6811f3e1926c447a034f915cea89e9b
Ivan Rozanov <navi.zor9@gmail.com>
2023-04-15 01:42:34 +0300

    fix change tracking in company repository

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -40,7 +40,7 @@ namespace Repository
 
         public Company GetCompany(Guid companyId, bool trackChanges)
         {
-            return FindByCondition(c => c.Id.Equals(companyId), false)
+            return FindByCondition(c => c.Id.Equals(companyId), trackChanges)
                 .SingleOrDefault();
         }
 
